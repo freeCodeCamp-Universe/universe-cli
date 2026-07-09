d096d5a723bd871c52f08a5d86664723b2f73b8c
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-07-09 11:45:30 +0200

    refactor: remove unused file

--- src/commands/create/package-manager/package-specifier.stub.ts
+++
@@ -1,9 +0,0 @@
-import type { PackageSpecifier } from "./package-specifier.port.js";
-
-class StubPackageSpecifier implements PackageSpecifier {
-  async specifyDeps(_projectDirectory: string): Promise<void> {
-    // No-op
-  }
-}
-
-export { StubPackageSpecifier };
