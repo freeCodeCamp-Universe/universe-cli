75c6c925f9b6f417a3348fe6dd2d2e537bbba1e3
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-08-03 10:21:35 +0200

    feat: add create-constellation-app alias for universe create

---
+++ bin/create-constellation-app.mjs
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { spawn } from "node:child_process";
+
+spawn(
+  process.execPath,
+  [
+    new URL("../dist/index.cjs", import.meta.url).pathname,
+    "create",
+    ...process.argv.slice(2),
+  ],
+  { stdio: "inherit" }
+);
