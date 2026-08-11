d88bf992dc983a9c26a1024be6e0575088b53602
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-08-11 21:42:30 +0200

    refactor: import package.json to get version (#68)

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,14 +1,8 @@
-import { readFileSync } from "node:fs";
 import { defineConfig } from "tsdown";
-
-const pkg = JSON.parse(readFileSync("package.json", "utf-8"));
 
 export default defineConfig({
   deps: {
     alwaysBundle: [/./],
-  },
-  define: {
-    __VERSION__: JSON.stringify(pkg.version),
   },
   dts: false,
   entry: ["src/index.ts"],
