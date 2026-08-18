a1223db5eca1354cbe08c0d603b2983820cb9a62
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2026-08-18 12:04:49 +0200

    refactor: share common 'drive' function

--- src/interaction/silent-driver.ts
+++ src/interaction/silent-driver.ts
@@ -1,12 +1,11 @@
+import { drive } from "./driver.js";
 import type { Step, StepResponse } from "./step.js";
 
+const silentHandler = async (step: Step): Promise<StepResponse> =>
+  step.type === "confirm" ? false : undefined;
+
 async function silentDrive<T>(gen: AsyncGenerator<Step, T, StepResponse>): Promise<T> {
-  let next = await gen.next();
-  while (!next.done) {
-    const step = next.value;
-    next = await gen.next(step.type === "confirm" ? false : undefined);
-  }
-  return next.value;
+  return drive(gen, silentHandler, () => {});
 }
 
 export { silentDrive };
